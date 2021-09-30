62641d9885123d86566f17802b63e756d37fd38b
uvsq21605636 <quentin.gruchet@ens.uvsq.fr>
2021-09-30 14:33:03 +0200

    add code for a fraction

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,2 +1,17 @@
 public class Fraction {
+    private double num = 1;
+    private double denum = 1;
+
+    Fraction(double num, double denum){
+        this.num = num;
+        this.denum = denum;
+    }
+
+    public String toString() {
+        return this.num + "/" + this.denum;
+    }
+
+    public double cal() {
+        return this.num / this.denum;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        System.out.print("Please enter a number for the numerator : ");
+        Scanner scanner = new Scanner(System.in);
+        String numerator = scanner.nextLine();
+
+        System.out.print("Please enter a number for the denuminator : ");
+        Scanner scanner2 = new Scanner(System.in);
+        String denum = scanner.nextLine();
+        Fraction fac = new Fraction(Double.parseDouble(numerator), Double.parseDouble(denum));
+        fac.toString();
+        System.out.print(fac + "=");
+        double res = fac.cal();
+        System.out.println(res);
+    }
 }
